ff075841066f4625878e55006b3860bd79a6b9e5
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-08-07 02:14:19 +0400

    Build.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,1 +1,31 @@
 
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+// const WebpackObfuscator = require('webpack-obfuscator');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[name].bundle.js',
+        // chunkFilename: '[name].chunk.js',
+    },
+    // optimization: {
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             commons: {
+    //                 filename: '[name].bundle.js',
+    //             },
+    //         },
+    //     },
+    // },
+    // plugins: [
+    //     new WebpackObfuscator(
+    //         {
+    //             rotateStringArray: true,
+    //             stringArray: true,
+    //             stringArrayThreshold: 0.75,
+    //         },
+    //         ['vendors.*.js', 'sw.js'],
+    //     ),
+    // ],
+});
