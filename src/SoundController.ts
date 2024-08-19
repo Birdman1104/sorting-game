b851026f97ce6b64b64dee35ec8e66a4c9ee2241
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-08-19 18:20:59 +0400

    Added match sound.

--- src/SoundController.ts
+++ src/SoundController.ts
@@ -2,6 +2,7 @@ import { lego } from '@armathai/lego';
 import { Howl } from 'howler';
 import { delayRunnable } from './Utils';
 import { audioAssets } from './assets/assetsNames/audio';
+import { BoardEvents } from './events/MainEvents';
 import { GameModelEvents } from './events/ModelEvents';
 
 class SoundControl {
@@ -11,7 +12,9 @@ class SoundControl {
     public constructor() {
         this.sounds = {};
 
-        lego.event.on(GameModelEvents.GameTimeUpdate, this.onTimerUpdate, this);
+        lego.event
+            .on(GameModelEvents.GameTimeUpdate, this.onTimerUpdate, this)
+            .on(BoardEvents.Match, this.onMatch, this);
     }
 
     public loadSounds(): void {
@@ -21,14 +24,18 @@ class SoundControl {
     }
 
     private onTimerUpdate(time: number): void {
-        if(time > 10000) return;
-        if(this.canPlayTick) {
+        if (time > 10000) return;
+        if (this.canPlayTick) {
             this.sounds.timer.play();
             this.canPlayTick = false;
             delayRunnable(1, () => {
                 this.canPlayTick = true;
-            })
+            });
         }
+    }
+
+    private onMatch(): void {
+        this.sounds.match.play();
     }
 }
 
