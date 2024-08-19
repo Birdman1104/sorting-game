bb48dc4326d3234c703467a2ea41b28d21fab87f
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-08-19 15:05:16 +0000

    Deploying to gh-pages from @ Birdman1104/sorting-game@32b47adc951f6936003fd4dc2d0a1fa46378d286 🚀

--- assets/assetsNames/audio.ts
+++ assets/assetsNames/audio.ts
@@ -1,1 +1,8 @@
-export const audioAssets: AssetNameAndPath[] = [{ name: 'timer', path: 'assets/audio/timer.mp3' }];
+export const audioAssets: AssetNameAndPath[] = [
+    { name: 'match', path: 'assets/audio/match.wav' },
+    { name: 'prize', path: 'assets/audio/prize.wav' },
+    { name: 'tap', path: 'assets/audio/tap.wav' },
+    { name: 'theme', path: 'assets/audio/theme.wav' },
+    { name: 'timer', path: 'assets/audio/timer.mp3' },
+    { name: 'wrongDrop', path: 'assets/audio/wrongDrop.wav' },
+];
