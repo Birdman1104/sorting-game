956b0eacff61f991c4e236297eeff51a7864024d
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-08-20 18:37:33 +0000

    Deploying to gh-pages from @ Birdman1104/sorting-game@65500c1b943fc0422f1293b168fcdd952edd2f02 🚀

--- assets/assetsNames/atlas.ts
+++ assets/assetsNames/atlas.ts
@@ -1,3 +1,1 @@
-export const atlases: { name: string; json: string; png: string }[] = [
-    { name: 'game', json: 'assets/atlas/game@1.png.json', png: 'assets/atlas/game.png' },
-];
+export const atlases: { name: string; json: string; png: string }[] = [];
