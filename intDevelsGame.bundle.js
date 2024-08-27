553280571f038f2e02bea1d58e92c86ec6d43afc
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-08-27 21:14:44 +0000

    Deploying to gh-pages from @ Birdman1104/sorting-game@b57d840f24a86e6357b31e843fdd5900b8ccc734 🚀

--- intDevelsGame.bundle.js
+++ intDevelsGame.bundle.js
@@ -35757,6 +35757,142 @@ const GameModelEvents = {
 const HeadModelEvents = { GameModelUpdate: 'HeadModelGameModelUpdate' };
 const ItemModelEvents = { TypeUpdate: 'ItemModelTypeUpdate' };
 
+;// CONCATENATED MODULE: ./node_modules/js-cookie/dist/js.cookie.mjs
+/*! js-cookie v3.0.5 | MIT */
+/* eslint-disable no-var */
+function js_cookie_assign (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+    for (var key in source) {
+      target[key] = source[key];
+    }
+  }
+  return target
+}
+/* eslint-enable no-var */
+
+/* eslint-disable no-var */
+var defaultConverter = {
+  read: function (value) {
+    if (value[0] === '"') {
+      value = value.slice(1, -1);
+    }
+    return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
+  },
+  write: function (value) {
+    return encodeURIComponent(value).replace(
+      /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
+      decodeURIComponent
+    )
+  }
+};
+/* eslint-enable no-var */
+
+/* eslint-disable no-var */
+
+function js_cookie_init (converter, defaultAttributes) {
+  function set (name, value, attributes) {
+    if (typeof document === 'undefined') {
+      return
+    }
+
+    attributes = js_cookie_assign({}, defaultAttributes, attributes);
+
+    if (typeof attributes.expires === 'number') {
+      attributes.expires = new Date(Date.now() + attributes.expires * 864e5);
+    }
+    if (attributes.expires) {
+      attributes.expires = attributes.expires.toUTCString();
+    }
+
+    name = encodeURIComponent(name)
+      .replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent)
+      .replace(/[()]/g, escape);
+
+    var stringifiedAttributes = '';
+    for (var attributeName in attributes) {
+      if (!attributes[attributeName]) {
+        continue
+      }
+
+      stringifiedAttributes += '; ' + attributeName;
+
+      if (attributes[attributeName] === true) {
+        continue
+      }
+
+      // Considers RFC 6265 section 5.2:
+      // ...
+      // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+      //     character:
+      // Consume the characters of the unparsed-attributes up to,
+      // not including, the first %x3B (";") character.
+      // ...
+      stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
+    }
+
+    return (document.cookie =
+      name + '=' + converter.write(value, name) + stringifiedAttributes)
+  }
+
+  function get (name) {
+    if (typeof document === 'undefined' || (arguments.length && !name)) {
+      return
+    }
+
+    // To prevent the for loop in the first place assign an empty array
+    // in case there are no cookies at all.
+    var cookies = document.cookie ? document.cookie.split('; ') : [];
+    var jar = {};
+    for (var i = 0; i < cookies.length; i++) {
+      var parts = cookies[i].split('=');
+      var value = parts.slice(1).join('=');
+
+      try {
+        var found = decodeURIComponent(parts[0]);
+        jar[found] = converter.read(value, found);
+
+        if (name === found) {
+          break
+        }
+      } catch (e) {}
+    }
+
+    return name ? jar[name] : jar
+  }
+
+  return Object.create(
+    {
+      set,
+      get,
+      remove: function (name, attributes) {
+        set(
+          name,
+          '',
+          js_cookie_assign({}, attributes, {
+            expires: -1
+          })
+        );
+      },
+      withAttributes: function (attributes) {
+        return js_cookie_init(this.converter, js_cookie_assign({}, this.attributes, attributes))
+      },
+      withConverter: function (converter) {
+        return js_cookie_init(js_cookie_assign({}, this.converter, converter), this.attributes)
+      }
+    },
+    {
+      attributes: { value: Object.freeze(defaultAttributes) },
+      converter: { value: Object.freeze(converter) }
+    }
+  )
+}
+
+var api = js_cookie_init(defaultConverter, { path: '/' });
+/* eslint-enable no-var */
+
+
+
 ;// CONCATENATED MODULE: ./src/backend/fetch.ts
 var fetch_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -35771,7 +35907,7 @@ var fetch_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _ar
 
 
 
-let cook;
+
 const fetchProductsData = () => fetch_awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield fetch(`${GLOBAL_DATA.URL}/api/products`);
@@ -35788,7 +35924,7 @@ const getPrize = () => fetch_awaiter(void 0, void 0, void 0, function* () {
         const response = yield fetch(`${GLOBAL_DATA.URL}/api/prizee`, {
             method: 'POST',
             body: JSON.stringify({
-                receipt: cook,
+                receipt: api.get('intGameReceipt'),
                 // receipt: Cookies.get('intGameReceipt'),
             }),
             headers: {
@@ -35805,13 +35941,11 @@ const getPrize = () => fetch_awaiter(void 0, void 0, void 0, function* () {
     }
 });
 const check = () => fetch_awaiter(void 0, void 0, void 0, function* () {
-    cook = Math.floor(Math.random() * 1000000);
-    console.log('cookie, ', cook);
     try {
         const response = yield fetch(`${GLOBAL_DATA.URL}/api/check`, {
             method: 'POST',
             body: JSON.stringify({
-                receipt: cook,
+                receipt: api.get('intGameReceipt'),
                 // receipt: Cookies.get('intGameReceipt'),
             }),
             headers: {
