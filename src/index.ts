2442b1eed9c673c687be7a141310405ad6777539
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-10-29 23:44:10 +0400

    WIP.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import App from './App';
 window.addEventListener('startIntDevelsGameStandalone', () => {
     window.game = new App();
     window.game.init();
+    // some random comment
 
     window.addEventListener('resize', () => window.game.appResize());
     window.addEventListener('orientationchange', () => window.game.appResize());
